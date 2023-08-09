b453206d3e0107988aad022b11586e13c7871422
chichibot <pupshy2019@gmail.com>
2023-08-09 06:47:18 -0400

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,62 @@
+import "../styles/globals.css";
+//import "@rainbow-me/rainbow/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import {
+	mainnet,
+	polygon,
+	optimism,
+	arbitrum,
+	goerli,
+	polygonMumbai,
+	optimismGoerli,
+	arbitrumGoerli,
+} from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import MainLayout from "../layout/mainLayout";
+
+const { chains, provider } = configureChains(
+	[
+		mainnet,
+		goerli,
+		polygon,
+		polygonMumbai,
+		optimism,
+		optimismGoerli,
+		arbitrum,
+		arbitrumGoerli,
+	],
+	[publicProvider({ apiKey: process.env.ALCHEMY_API_KEY }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+	appName: "My Alchemy DApp",
+	chains,
+});
+
+const wagmiClient = createClient({
+	autoConnect: true,
+	connectors,
+	provider,
+	projectID:'4bc398e205814bd1362798d4f39e68b8'
+});
+
+export { WagmiConfig, RainbowKitProvider };
+function MyApp({ Component, pageProps }) {
+	return (
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider
+				modalSize="compact"
+				initialChain={process.env.NEXT_PUBLIC_DEFAULT_CHAIN}
+			>
+				<MainLayout>
+					<Component {...pageProps} />
+				</MainLayout>
+			</RainbowKitProvider>
+		</WagmiConfig>
+	);
+}
+
+export default MyApp;
